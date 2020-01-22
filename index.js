c4116157befeacce78bbc84210e63f7778c678d3
manel turki <menelturki@gmail.com>
2020-01-22 18:10:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let gifts=["manel","ahmet","selim"];
+ 
+function writeCards( gifts, event ) {
+ 
+  for ( let i = 0; i < gifts.length; i++ ) {
+    console.log( `Thank you, ${gifts[i]}, for the wonderful ${event} gift!` )
+  }
+  return gits;
+}
+writeCards(gifts,"birthday");
+function countdown( n ) {
+  while ( n > 0 ) {
+    console.log( n );
+    n --;
+  }
+  console.log( n );
+}
